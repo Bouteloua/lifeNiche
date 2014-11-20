4e37e02efac20be755070ea687fbbe4175d517a1
Bouteloua <Franzone89@gmail.com>
2014-11-19 22:04:17 -0500

    added a switch

--- controller/mapper.py
+++ controller/mapper.py
@@ -9,101 +9,35 @@ from password import *
 
 def main():
 	#This is a hello
-	#Setup your client ID and password in the file named password.py
-	cl = LMClient(userId=userName, pwd=password)
+	func1()
 
 	#Set the algorithm to be used niche model(s)
 	alg = cl.sdm.getAlgorithmFromCode('ATT_MAXENT')
 
-	postDicLayers = rawMetaData()
+	run = True
+	while run:
+		inputTypeAndLayer = raw_input('Do you wish to rerun post typecodes and post layers or use past pickle IDs ("new" or "old" new): ')
+		if inputTypeAndLayer.lower() == 'new':
+			postDicLayers = rawMetaData()
+			postTypeCode(postDicLayers)
+			postLayers(postDicLayers)
+			run = False
+		elif inputTypeAndLayer == 'old' or inputTypeAndLayer == 'old':
+			lyrs, occurs, scens = pickleFiles()
+			run = False
+		else:
+			run = True
 
-############################## POST TYPECODE #######################################
-		# postTypeCode(
-		# 	code-> The code to use for this new type code [string]
-		# 	title-> (optional) A title for this type code [string]
-		# 	description -> (optional) An extended description of this type code [string]
-		# 	)
-	for typecode in postDicLayers['CURR'].keys():
-		cl.sdm.postTypeCode(code=typecode, title=typecode.upper(), description=postDicLayers['CURR'][typecode]['typeCodeDescription'])
-############################## POST TYPECODE #######################################
+	print lyrs, occurs, scens
 
-
-############################## POST LAYERS #########################################
-	for key, layerNames in postDicLayers.iteritems():
-		for key1, layerName in layerNames.items():
-			lyrObj = cl.sdm.postLayer(name=layerName['Name'], epsgCode=layerName['epsgCode'], envLayerType=layerName['envLayerType'], units=layerName['units'], dataFormat=layerName['dataFormat'], fileName=layerName['fileName'], title=layerName['title'], description=layerName['layerDescription'])
-			postDicLayers[key][key1].update({'lyrID':lyrObj.id})
-	
-
-	#Save a pickle file of the layer IDs 
-	with open('../views/pickleDic/' + 'layerName_dict' + '.pickle', 'wb') as f:
-		cPickle.dump(postDicLayers, f)
-
-	print '####### Start of the layer ID dictionary ############'
-	print postDicLayers
-
-############################## END POST LAYERS ########################################
+	#scenarioDic = postScenario(postDicLayers)
 
 
 
-############################## POST OCCURRENCE ########################################
-	occurrenceDic = csvToShapefile()
-	for key, value in occurrenceDic.iteritems():
-		occObj = cl.sdm.postOccurrenceSet(displayName=str(key), fileType='shapefile', fileName=value['shpPath'], epsgCode=value['epsgCode'])
-
-		#Add the occurrence ID from lifemapper to the dictionary 
-		occurrenceDic[key].update({'occID':occObj.id})
-	
-	#Save a pickle file of the occurrence IDs 
-	with open('../views/pickleDic/' + 'occurrence_dict' + '.pickle', 'wb') as f:
-		cPickle.dump(occurrenceDic, f)
-
-	print '####### Start of the occurrence ID dictionary ############'
-	print occurrenceDic
-
-############################## END POST OCCURRENCE ####################################
 
 
 
-############################## POST Scenario ########################################
 
-
-	scenarioDic = dict() 
-	count = 1
-	resolution = 0
-	units = 0
-	epsgCode = 0
-	for key, layerNames in postDicLayers.iteritems():
-		hold = []
-		for key1, layerName in layerNames.items():
-			hold.append(postDicLayers[key][key1]['lyrID'])
-			resolution = postDicLayers[key][key1]['resolution']
-			units = postDicLayers[key][key1]['units']
-			epsgCode = postDicLayers[key][key1]['epsgCode']
-
-		keyDic = 'base' + str(count) + '_' + key
-		scenarioDic.setdefault(keyDic, {
-			# remove the index + X
-			'layers': hold,
-			'code': keyDic,
-			'epsgCode': epsgCode,
-			'units': units,
-			'title': 'Climate Scenario ' + key,
-			'author': 'CGW',
-			'resolution': resolution,
-			'bioclim': key
-			})
-		scn = cl.sdm.postScenario(layers=hold, code=keyDic, epsgCode=epsgCode, units=units, title=key + 'Climate Scenario', author="CGW", resolution=resolution)
-		scenarioDic[keyDic].update({'scnID':scn.id})
-
-			#Save a pickle file of the occurrence IDs 
-	with open('../views/pickleDic/' + 'scenario_dict' + '.pickle', 'wb') as f:
-		cPickle.dump(scenarioDic, f)
-
-	print '####### Start of the Scenario ID dictionary ############'
-	print scenarioDic
-
-############################## END POST Scenario ####################################
 
 ############################## END POST Experiment ####################################
 
@@ -141,10 +75,117 @@ def main():
 # 	exp = cl.sdm.postExperiment(algorithm=alg, mdlScn=968, occSetId=5645771, prjScns=[968, 969], name='Test Experiment', description='Test Experiment on Cakile and Present and Future Bio1')
 ############################################################################################
 
+def func1():
+	#Setup your client ID and password in the file named password.py
+    global cl
+    cl = LMClient(userId=userName, pwd=password)
+
+
+def postTypeCode(postDicLayers):
+############################## POST TYPECODE #######################################
+		# postTypeCode(
+		# 	code-> The code to use for this new type code [string]
+		# 	title-> (optional) A title for this type code [string]
+		# 	description -> (optional) An extended description of this type code [string]
+		# 	)
+	for typecode in postDicLayers['CURR'].keys():
+		cl.sdm.postTypeCode(code=typecode, title=typecode.upper(), description=postDicLayers['CURR'][typecode]['typeCodeDescription'])
+############################## POST TYPECODE #######################################
+
+def postOccurrence():
+############################## POST OCCURRENCE ########################################
+	occurrenceDic = csvToShapefile()
+	for key, value in occurrenceDic.iteritems():
+		occObj = cl.sdm.postOccurrenceSet(displayName=str(key), fileType='shapefile', fileName=value['shpPath'], epsgCode=value['epsgCode'])
+
+		#Add the occurrence ID from lifemapper to the dictionary 
+		occurrenceDic[key].update({'occID':occObj.id})
+	
+	#Save a pickle file of the occurrence IDs 
+	with open('../views/pickleDic/' + 'occurrence_dict' + '.pickle', 'wb') as f:
+		cPickle.dump(occurrenceDic, f)
+
+	print '####### Start of the occurrence ID dictionary ############'
+	print occurrenceDic
+	return occurrenceDic
+############################## END POST OCCURRENCE ####################################
+
+def postLayers(postDicLayers):
+	############################## POST LAYERS #########################################
+	for key, layerNames in postDicLayers.iteritems():
+		for key1, layerName in layerNames.items():
+			lyrObj = cl.sdm.postLayer(name=layerName['Name'], epsgCode=layerName['epsgCode'], envLayerType=layerName['envLayerType'], units=layerName['units'], dataFormat=layerName['dataFormat'], fileName=layerName['fileName'], title=layerName['title'], description=layerName['layerDescription'])
+			postDicLayers[key][key1].update({'lyrID':lyrObj.id})
+	
+
+	#Save a pickle file of the layer IDs 
+	with open('../views/pickleDic/' + 'layerName_dict' + '.pickle', 'wb') as f:
+		cPickle.dump(postDicLayers, f)
+
+	print '####### Start of the layer ID dictionary ############'
+	print postDicLayers
+	return postDicLayers
+
+############################## END POST LAYERS ########################################
+
+def postScenario(postDicLayers):
+	############################## POST Scenario ########################################
+	scenarioDic = dict() 
+	count = 1
+	resolution = 0
+	units = 0
+	epsgCode = 0
+	for key, layerNames in postDicLayers.iteritems():
+		hold = []
+		for key1, layerName in layerNames.items():
+			hold.append(postDicLayers[key][key1]['lyrID'])
+			resolution = postDicLayers[key][key1]['resolution']
+			units = postDicLayers[key][key1]['units']
+			epsgCode = postDicLayers[key][key1]['epsgCode']
+
+		keyDic = 'base' + str(count) + '_' + key
+		scenarioDic.setdefault(keyDic, {
+			# remove the index + X
+			'layers': hold,
+			'code': keyDic,
+			'epsgCode': epsgCode,
+			'units': units,
+			'title': 'Climate Scenario ' + key,
+			'author': 'CGW',
+			'resolution': resolution,
+			'bioclim': key
+			})
+		scn = cl.sdm.postScenario(layers=hold, code=keyDic, epsgCode=epsgCode, units=units, title=key + 'Climate Scenario', author="CGW", resolution=resolution)
+		scenarioDic[keyDic].update({'scnID':scn.id})
+
+			#Save a pickle file of the occurrence IDs 
+	with open('../views/pickleDic/' + 'scenario_dict' + '.pickle', 'wb') as f:
+		cPickle.dump(scenarioDic, f)
+
+	print '####### Start of the Scenario ID dictionary ############'
+	print scenarioDic
+	return scenarioDic
+
+############################## END POST Scenario ####################################
+
+
+def pickleFiles():
+	with open('../views/pickleDic/layerName_dict.pickle') as f:
+		lyrs = cPickle.load(f)
+
+	with open('../views/pickleDic/occurrence_dict.pickle') as f:
+		occurs = cPickle.load(f)
+
+	with open('../views/pickleDic/scenario_dict.pickle') as f:
+		scens = cPickle.load(f)
+	return lyrs, occurs, scens
+
+
+
 #Creates the data structures for the layers 
 def rawMetaData():
 	#If you rerun increase this number to avoid unique id collisions  
-	add = 740
+	add = 875
 	#Returns all the gtiff files in the folder GTiff
 	folderFiles = glob.glob('../views/GTiff/*.tif')
 
